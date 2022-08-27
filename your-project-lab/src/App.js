8f269170371c6bae7f7f31711d4a375b8440f4a8
RustinParsenNC <Rustincode@gmail.com>
2022-08-26 21:26:31 -0400

    initial checking

--- your-project-lab/src/App.js
+++ your-project-lab/src/App.js
@@ -2,9 +2,21 @@
 import './App.css';
 
 function App() {
+
+
+
+
+
   return (
-    <div className="App">
-      test
+    <div className="s1">
+     <div className="s2">
+       <button>S2</button>
+       <button>S2</button>
+       <button>S2</button>
+     </div>
+
+     <div className="s3">s3</div>
+     {/* <div className="s2">Hello</div> */}
     </div>
   );
 }
