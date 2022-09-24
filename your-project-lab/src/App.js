52d0300150ca68f1988f0e1b14bce7e84100194b
RustinParsenNC <Rustincode@gmail.com>
2022-09-23 20:25:25 -0400

    Update App.js

--- your-project-lab/src/App.js
+++ your-project-lab/src/App.js
@@ -1,5 +1,5 @@
 
-import React from "react";
+import React,{useState} from "react";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 import Button from "react-bootstrap/Button";
 
@@ -7,6 +7,16 @@ import './App.css';
 
 
 function App() {
+  const [temp, setTemp] = useState("hello");
+
+
+  const showMultipli = () => {
+  
+    setTemp("hello from multipli");
+    // alert("You are in Multipli")
+
+
+  }
 
 
 
@@ -17,11 +27,13 @@ function App() {
       <div className="s4">
       
       <div className="s3">
-      <Button className="s5" variant="success">Multipli</Button>
+      <Button onClick= {showMultipli} className="s5" variant="success">Multipli</Button>
+      <Button className="s5" variant="success">About</Button>
    
       </div>
       <div className="s2">
-        dfs
+        {temp}
+        
       </div>
 
       
