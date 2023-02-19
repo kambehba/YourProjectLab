c67320481fc853fc190fd10b7ab13f4358b500b2
RustinParsenNC <Rustincode@gmail.com>
2023-02-19 17:32:40 -0500

    finished project

--- your-project-lab/src/App.js
+++ your-project-lab/src/App.js
@@ -4,6 +4,7 @@ import ButtonGroup from "react-bootstrap/ButtonGroup";
 import Button from "react-bootstrap/Button";
 import Multipli from "./Componenets/Multipli/Multiply";
 import About from "./Componenets/About/About";
+import Demo from "./Componenets/Demo/Demo";
 import './App.css';
 
 
@@ -17,21 +18,26 @@ function App() {
   
   const [showMultipli, setShowMultipli] = useState(false);
   const [showAbout, setShowAbout] = useState(true);
-
+  const [showDemo, setShowDemo] = useState(false);
   const handleAboutClick = () => {
   
     
    setShowAbout(true);
    setShowMultipli(false);
-
+   setShowDemo(false);
 
   }
-
+  
+  const handleDemoClick = () => {
+    setShowAbout(false);
+    setShowMultipli(false);
+    setShowDemo(true);
+  };
   const handleMultipliClick = () => {
   
    setShowAbout(false);
    setShowMultipli(true);
-    
+   setShowDemo(false);
    }
    if(showMultipli){
 
@@ -45,6 +51,13 @@ function App() {
 
  }
 
+ if (showDemo) {
+  mainContent = (
+    <div>
+      <Demo />
+    </div>
+  );
+}
 
   return (
     <div>
@@ -54,6 +67,9 @@ function App() {
       
       <Button onClick= {handleMultipliClick} className="s5" variant="Secondary">Multipli</Button>
       <Button onClick= {handleAboutClick} className="b2" variant="Secondary">About</Button>
+      <Button onClick={handleDemoClick} className="b2" variant="Secondary">
+            Demo
+          </Button>
    
       </div>
       <div className="s2">
--- your-project-lab/src/Componenets/About/About.js
+++ your-project-lab/src/Componenets/About/About.js
@@ -1,13 +1,52 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState } from "react";
+import headshot from "../../images/i3.png";
+import wo from "../../images/i2.jpg";
+import v1 from "../../images/v2.mp4";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Button from "react-bootstrap/Button";
+import "./About.css";
 
 
+function About() {
+  const [mainContent, setMainContent] = useState("");
 
-function About(){
-
-
-
-
-    return(<div>hello from About</div>);
+  return (
+    <div className="about-s1">
+      <div className="about-s2">
+        <h1 className="about-s4">Bio</h1>
+        <div className="about-s3">
+          <img src={headshot}></img>
+          <img className="about-s6" src={wo}></img>
+          <p className="about-s5">
+            <h4>Rustin Parsen</h4>
+            15 Years Old <br />
+            West Orange High School (Sophomore) <br />
+            <br />
+            Born in Indianapolis , Indiana <br />
+            Live in Orlando Florida
+          </p>
+        </div>
+      </div>
+      <div>
+        <h1 className="about-s4">Interests</h1>
+        <p className="about-s5">
+          <h4>Video Gameing</h4>
+          <h4>Computer Science</h4>
+          <h4>New Technologies </h4>
+          <h4>Clay Craft</h4>
+          <h4>Scuba Diving</h4>
+        </p>
+      </div>
+      <div>
+        <h1 className="about-s4">Futuer Plans</h1>
+        <p className="about-s5">
+          <h4>Expand my knowledge on Computer Science</h4>
+          <h4>Develop more Software Solutions on this portal</h4>
+          <h4>Learn More about Internet of Things (IoT)</h4>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default About;
